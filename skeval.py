6821133d0e57feaa7cd757660123535dd1341c2b
Eli Levasseur <eliplevasseur7@gmail.com>
2026-02-28 13:50:39 -0800

    refactor: reduce duplication in Model class with helper methods
    Extract common matrix operations into reusable helpers and optimize error_rate
    calculation.

--- skeval.py
+++ skeval.py
@@ -7,52 +7,53 @@ class Model:
         self.matrix = matrix
         self.results = {}
 
+    def _extract_matrix_values(self):
+        """Extract confusion matrix values: tn, fp, fn, tp."""
+        tn = self.matrix.iloc[0, 0]
+        fp = self.matrix.iloc[0, 1]
+        fn = self.matrix.iloc[1, 0]
+        tp = self.matrix.iloc[1, 1]
+        return tn, fp, fn, tp
+
+    def _get_total_samples(self):
+        """Calculate total number of samples."""
+        tn, fp, fn, tp = self._extract_matrix_values()
+        return tn + fp + fn + tp
 
     def __repr__(self):
-        return f"{self.model=}"
+        return f"This object can perform a series of operations to evaluate a models performance"
 
+    def __str__(self):
+        return f"{self.model=}"
 
     def sensitivity(self):
         """Calculates the sensitivity (true positive rate) from the stored confusion matrix."""
-        tp = self.matrix.iloc[1, 1]
-        fn = self.matrix.iloc[1, 0]
-        sensitivity = tp / (tp + fn)
-        self.results['sensitivity'] = sensitivity
+        _, _, fn, tp = self._extract_matrix_values()
+        self.results['sensitivity'] = tp / (tp + fn)
 
 
     def specificity(self):
         """Calculates the specificity (true negative rate) from the stored confusion matrix."""
-        tn = self.matrix.iloc[0, 0]
-        fp = self.matrix.iloc[0, 1]
-        specificity = tn / (tn + fp)
-        self.results['specificity'] = specificity
+        tn, fp, _, _ = self._extract_matrix_values()
+        self.results['specificity'] = tn / (tn + fp)
     
 
     def accuracy(self):
         """Calculates the accuracy from the stored confusion matrix."""
-        tp = self.matrix.iloc[1, 1]
-        tn = self.matrix.iloc[0, 0]
-        fp = self.matrix.iloc[0, 1]
-        fn = self.matrix.iloc[1, 0]
-        accuracy = (tp + tn) / (tp + tn + fp + fn)
-        self.results['accuracy'] = accuracy
+        tn, fp, fn, tp = self._extract_matrix_values()
+        self.results['accuracy'] = (tp + tn) / self._get_total_samples()
     
 
     def error_rate(self):
         """Calculates the error rate from the stored confusion matrix."""
-        tp = self.matrix.iloc[1, 1]
-        tn = self.matrix.iloc[0, 0]
-        fp = self.matrix.iloc[0, 1]
-        fn = self.matrix.iloc[1, 0]
-        error_rate = (fp + fn) / (tp + tn + fp + fn)
-        self.results['error_rate'] = error_rate
+        if 'accuracy' not in self.results:
+            self.accuracy()
+        self.results['error_rate'] = 1 - self.results['accuracy']
 
-    def overall_model_cost(self, matrix, cost_fp, cost_fn):
+    def overall_model_cost(self, cost_fp, cost_fn):
         """Calculates the overall model cost based on false positives and false negatives."""
-        fp = matrix.iloc[0, 1]
-        fn = matrix.iloc[1, 0]
-        overall_cost = (fp * cost_fp) + (fn * cost_fn)
-        self.results['overall_model_cost'] = overall_cost
+        _, fp, fn, _ = self._extract_matrix_values()
+        self.results['overall_model_cost'] = (fp * cost_fp) + (fn * cost_fn)
 
     def get_results(self):
         """Returns all computed metric values."""
